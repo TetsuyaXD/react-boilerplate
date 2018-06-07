93f0a1eabef6de5e9502a927840db827421456e7
TetsuyaXD <jamie.kolles@axelspringer.de>
2018-06-07 16:35:24 +0200

    Added react-router

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import './index.css';
 import { Provider } from "react-redux";
 import configureStore from "./store/index";
 import App from './App';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-            <App />
+        <Router>
+            <Route exact path='/' component={App}/>
+        </Router>
     </Provider>, document.getElementById('root'));
